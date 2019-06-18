f80cde65cac2dc54c8333db7ce7cbe112daf02d6
Dinesh Katwal <dinesh.katwal@logicabeans.com>
2019-06-18 17:43:52 +0545

    Initial Commit

---
+++ vars/sendNotifications.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+def call (String buildStatus = 'Started') {
+     // build status of null means successful
+    buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+    // Default values
+    def color = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+    def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESSFUL') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    }
+
+    // // Send notifications
+    // slackSend (color: colorCode, message: summary)
+    // hipchatSend (color: color, notify: true, message: summary)
+
+    emailext (
+        to: 'medineshkatwal@gmail.com',
+        subject: subject,
+        body: details,
+        recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+        )
+}
