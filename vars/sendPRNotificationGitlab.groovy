383a632ce89abf6873086d341f4b50e50f6a7149
Dinesh Katwal <dinesh.katwal@logicabeans.com>
2019-06-18 20:31:13 +0545

    Added sendPRNotficationGitlab.groovy

--- vars/sendPRNotificationGitlab.groovy
+++ vars/sendPRNotificationGitlab.groovy
@@ -1,13 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String name="CI Jenkins", String state) {
-    if(state == "pending") {
-        post {
-            always {
-                updateGitlabCommitStatus name: name, state: state
-            }
-        }
-    }
-    
-    
+        updateGitlabCommitStatus name: name, state: state   
 }
