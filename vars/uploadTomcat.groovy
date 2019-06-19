645012dd3615230bed2cd764d511aa6e3117d17e
Dinesh Katwal <dinesh.katwal@logicabeans.com>
2019-06-19 17:37:22 +0545

    remove doublequote

--- vars/uploadTomcat.groovy
+++ vars/uploadTomcat.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(Map param) {
-    sshPut remote: "${param.remote}", from: "${param.propsFrom}", into: "${param.propsTo}"
-    sshPut remote: "${param.remote}", from: "${param.artifactFrom}", into: "${param.artifactTo}"
+    sshPut remote: param.remote, from: "${param.propsFrom}", into: "${param.propsTo}"
+    sshPut remote: param.remote, from: "${param.artifactFrom}", into: "${param.artifactTo}"
 }
