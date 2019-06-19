de95cfaf229b94e2fdefc3c620b55cf9bfe1d1fa
Dinesh Katwal <dinesh.katwal@logicabeans.com>
2019-06-19 17:08:04 +0545

    Appended tar -x in gitArchieve

--- vars/gitArchieve.groovy
+++ vars/gitArchieve.groovy
@@ -1,4 +1,4 @@
 
 def call(Map param) {
-    sh "git archive --remote=git@'${param.repo}' HEAD:'${param.path}' '${param.fileName}'"
+    sh "git archive --remote=git@'${param.repo}' HEAD:'${param.path}' '${param.fileName}' | tar -x"
 }
