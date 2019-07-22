4a0b6e243947f61c62214590d308670ee180c1d3
rraj <rraj@f1soft.com>
2019-07-22 14:15:32 +0545

    propsFrom fixed as optional parameter

--- vars/uploadTomcat.groovy
+++ vars/uploadTomcat.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(Map param) {
-    param.remote.allowAnyHosts = true
-    if(param.propsFrom}) {
+    param.remote.allorwAnyHosts = true
+    if(param.propsFrom?.trim()}) {
     	sshPut remote: param.remote, from: "${param.propsFrom}", into: "${param.propsTo}"
     }
     sshPut remote: param.remote, from: "${param.artifactFrom}", into: "${param.artifactTo}"
