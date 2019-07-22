7c87a7937b8739d173ff12f5f8e7233143f8f61d
Dinesh katwal <medineshkatwal@gmail.com>
2019-07-22 06:27:39 -0700

    Update sendPRNotificationGitlab.groovy

--- vars/sendPRNotificationGitlab.groovy
+++ vars/sendPRNotificationGitlab.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String state, String name="CI Jenkins" ) {
-        updateGitlabCommitStatus name: name, state: state   
+        updateGitlabCommitStatus name: name, state: state.toLowerCase()   
 }
