f39b937bfe93ee2f4768337daf59258a6b4c3e6c
Dinesh Katwal <medineshkatwal@gmail.com>
2019-07-30 13:03:37 +0545

    set default slack notification on

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 
 def call (Map param) {
+
     // Default values
     def color = 'RED'
+    def slack_notification=param.slack?:true
     def colorCode = '#FF0000'
     def subject = "${param.status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'${param.title?:""}"
     def summary = "${subject} (${env.BUILD_URL})"
@@ -19,7 +21,7 @@ def call (Map param) {
     }
 
     // // Send notifications
-    if(param.slack==true) {
+    if(slack_notification==true) {
         slackSend (color: colorCode, message: summary)
     }
     // hipchatSend (color: color, notify: true, message: summary)
