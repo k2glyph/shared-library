798912451503f0565578009a729dbf518783346d
Dinesh katwal <medineshkatwal@gmail.com>
2019-10-17 07:10:01 +0530

    Create deployKubernetes.groovy

---
+++ vars/deployKubernetes.groovy
@@ -0,0 +1,11 @@
+def call(Map param) {
+   container("gcloud") {
+     withCredentials([file(credentialsId:param.credential_id, variable: 'credential')]) {
+       sh("gcloud auth activate-service-account --key-file=${credential}")
+       sh("gcloud container clusters get-credentials ${param.cluster_name} --zone param.zone --project ${param.project_name}")
+     }
+     script {
+        sh "kubectl -n ${param.namespace} set image deployment.v1.apps/${param.deployment} '${param.deployment}=${imageTag}'"
+     }
+   }
+}
