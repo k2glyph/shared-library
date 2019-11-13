826ac07f0ccb28037b255101feb2e94419840592
Dinesh katwal <medineshkatwal@gmail.com>
2019-11-13 14:22:07 +0530

    Create backup-restore-cloudsql.groovy

---
+++ vars/backup-restore-cloudsql.groovy
@@ -0,0 +1,14 @@
+def call(Map param) {
+  if(param.backup=="true") {
+    sh "echo\"Requesting gcloud to create backup---\n\""
+    sh """
+      gcloud sql backups create --instance=${param.instance} --project ${param.project}
+    """
+   }
+   if(param.restore=="true") {
+      sh "echo\"Requesting gcloud to restore from latest backup---\n\""
+      sh """
+        gcloud sql backups restore $(gcloud sql backups list --instance=$instance --project $projet | grep "SUCCESSFUL" | awk 'NR==1{print $1}') --restore-instance=$instance --backup-instance=$instance
+      """
+   }
+}
