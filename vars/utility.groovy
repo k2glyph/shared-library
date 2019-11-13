64f00402aba58d63a1f89b3272c9ad6e2c4b7a0b
Dinesh katwal <medineshkatwal@gmail.com>
2019-11-13 16:31:43 +0530

    Update utility.groovy

--- vars/utility.groovy
+++ vars/utility.groovy
@@ -1,6 +1,6 @@
 def call(Map param) {
   if(param.check=="jobs") {
     sh "echo \"Checking migrate job status :${param}\""
-    def result=sh(returnStdout: true, script: 'kubectl -n ${param.namespace} wait --for=condition=complete --timeout=50s jobs/\$(kubectl get jobs -n ${param.namespace} | grep ${param.grep} | awk \'NR==1{print \$1}\') | cut -d" " -f2,3').trim()
+    def result=sh(returnStdout: true, script: "kubectl -n ${param.namespace} wait --for=condition=complete --timeout=50s jobs/\$(kubectl get jobs -n ${param.namespace} | grep ${param.grep} | awk 'NR==1{print \$1}') | cut -\d" \" -f2,3").trim()
   }
 }
