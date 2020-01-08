714c4ebec06cd798b8af2d30fb92421659f88148
Dinesh katwal <medineshkatwal@gmail.com>
2020-01-08 12:25:52 +0530

    Update deployKubernetes.groovy

--- vars/deployKubernetes.groovy
+++ vars/deployKubernetes.groovy
@@ -4,6 +4,14 @@ def call(Map param) {
        sh("gcloud auth activate-service-account --key-file=${credential}")
         sh("gcloud container clusters get-credentials ${param.cluster_name} --zone ${param.zone_name} --project ${param.project_name}")
      }
+     if (param.type == 'MIGRATE') {
+        sh """
+           export SECRET_NAME=\$(kubectl -n ${param.namespace} get secrets | grep ${param.grep} | awk '{print \$1}')
+           export CI_COMMIT_SHA=${param.version}
+           envsubst < ci/${param.job}.yaml | kubectl -n ${param.namespace} create -f -
+       """
+       return
+     }
      script {
         sh "kubectl -n ${param.namespace} set image deployment.v1.apps/${param.deployment} '${param.deployment}=${param.imageTag}'"
      }
