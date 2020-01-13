9d624b575a0f45880ae5d154944f395cfd2e0dd5
Dinesh katwal <medineshkatwal@gmail.com>
2020-01-13 11:43:04 +0530

    Update loginKubernetes.groovy

--- vars/loginKubernetes.groovy
+++ vars/loginKubernetes.groovy
@@ -1,5 +1,6 @@
 def call(Map param) {
    container("gcloud") {
+     println("Login into kubernetes cluster: ${param.cluster_name}")
      withCredentials([file(credentialsId:param.credential_id, variable: 'credential')]) {
        sh("gcloud auth activate-service-account --key-file=${credential}")
        sh("gcloud container clusters get-credentials ${param.cluster_name} --zone ${param.zone_name} --project ${param.project_name}")
