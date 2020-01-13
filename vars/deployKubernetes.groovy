7a305a454fe85b07353cc86933953b7c3958542d
Dinesh katwal <medineshkatwal@gmail.com>
2020-01-13 12:38:39 +0530

    Update deployKubernetes.groovy

--- vars/deployKubernetes.groovy
+++ vars/deployKubernetes.groovy
@@ -16,6 +16,7 @@ def call(Map param) {
           secret_name=sh(returnStdout: true, script: "kubectl -n ${param.namespace} get secrets | grep ${param.grep} | awk '{print \$1}'")
         }
         sh """
+           export DB_HOST=${param.db_host}
            export SECRET_NAME=${secret_name}
            export CI_COMMIT_SHA=${param.version}
            envsubst < ci/${param.job}.yaml | kubectl -n ${param.namespace} create -f -
