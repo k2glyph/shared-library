d5ff70721c4b6c85e79e49db8c16338f19e6d19f
Dinesh katwal <medineshkatwal@gmail.com>
2020-01-13 12:20:28 +0530

    Update deployKubernetes.groovy

--- vars/deployKubernetes.groovy
+++ vars/deployKubernetes.groovy
@@ -6,19 +6,17 @@ def call(Map param) {
            sh("gcloud container clusters get-credentials ${param.cluster_name} --zone ${param.zone_name} --project ${param.project_name}")
         }
       }  
-
+     
      if (param.type == 'MIGRATE') {
         println("Migration job running")
+        def secret_name=""   
         if(param.org) {
-            sh """
-               export SECRET_NAME=\$(kubectl -n ${param.namespace} get secrets -l org=${param.org} -o jsonpath="{.items[0].metadata.name}")
-            """           
+          secret_name=sh(returnStdout: true, script: "kubectl -n ${param.namespace} get secrets -l org=${param.org} -o jsonpath=\"{.items[0].metadata.name}\"")
         }else {
-           sh """
-               export SECRET_NAME=\$(kubectl -n ${param.namespace} get secrets | grep ${param.grep} | awk '{print \$1}')
-           """
+          secret_name=sh(returnStdout: true, script: "kubectl -n ${param.namespace} get secrets | grep ${param.grep} | awk '{print \$1}'"
         }
         sh """
+           export SECRET_NAME=${secret_name}
            export CI_COMMIT_SHA=${param.version}
            envsubst < ci/${param.job}.yaml | kubectl -n ${param.namespace} create -f -
         """
