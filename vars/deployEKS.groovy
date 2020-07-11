3ec53dfb0dd16a27cf17caaa2631bdbfc61840e6
Dinesh katwal <medineshkatwal@gmail.com>
2020-07-11 16:28:00 +0530

    Update deployEKS.groovy

--- vars/deployEKS.groovy
+++ vars/deployEKS.groovy
@@ -3,15 +3,15 @@ def call(Map param) {
         println("Migration job running")
         def secret_name=""   
         if(param.org) {
-           secret_name=sh(returnStdout: true, script: "kubectl --kubeconfig ${param.kubeconfig} -n ${param.namespace} get secrets --sort-by=.metadata.creationTimestamp | grep secret | tail -1 | awk '{print \$1}'")
+           secret_name=sh(returnStdout: true, script: "kubectl -n ${param.namespace} get secrets --sort-by=.metadata.creationTimestamp | grep secret | tail -1 | awk '{print \$1}'")
         }else {
-          secret_name=sh(returnStdout: true, script: "kubectl --kubeconfig ${param.kubeconfig} -n ${param.namespace}  get secrets --sort-by=.metadata.creationTimestamp | grep secret | grep ${param.grep} | tail -1 | awk '{print \$1}'")
+          secret_name=sh(returnStdout: true, script: "kubectl -n ${param.namespace}  get secrets --sort-by=.metadata.creationTimestamp | grep secret | grep ${param.grep} | tail -1 | awk '{print \$1}'")
         }
         sh """
            export DB_HOST=${param.db_host}
            export SECRET_NAME=${secret_name}
            export CI_COMMIT_SHA=${param.version}
-           envsubst '\${SECRET_NAME},\${CI_COMMIT_SHA},\${DB_HOST}' < ci/${param.job}.yaml | kubectl --kubeconfig ${param.kubeconfig} -n ${param.namespace} create -f -
+           envsubst '\${SECRET_NAME},\${CI_COMMIT_SHA},\${DB_HOST}' < ci/${param.job}.yaml | kubectl -n ${param.namespace} create -f -
         """
        return
      }
